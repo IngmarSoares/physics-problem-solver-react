d71a2f12b6780c1d8378c35c524f31f10d1b815a
Ingmar Manuel Soares Medina <ingmar.soares.medina@gmail.com>
2022-10-11 12:34:29 +0200

    Added react-router-dom and simple routes for main and about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1> Physics problem solver</h1>
-      <p>In this app, you are going to solve your school physics problems!!</p>
-      <p> Please, use it only to check your solutions, if you want to learn you should try to solve the problems yourself before using this!</p>
+      <BrowserRouter>
+      
+        <Routes>
+          <Route path='/' element={ <Home/>} />
+          <Route path='/about' element={ <About/> } />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
